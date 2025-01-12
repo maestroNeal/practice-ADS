03f326f3b0d239ab257c3fa0f72acc0228bfa502
Rahul Gangwar <neorahul@outlook.in>
2025-01-12 20:24:17 +0530

    half tringle

--- Check/test.cpp
+++ Check/test.cpp
@@ -9,14 +9,27 @@ void square(int x){
         }
         cout<<"\n";
     }
-
 }
-
-
+void rightTriHypRightSameNum(int x){
+    for(int i=0;i<x;i++){
+        for (int j=0; j<i; j++){
+            cout<<i<<" ";
+        }
+        cout<<"\n";
+    }
+}
+void rightTriHypRightDownSameNum(int n){
+    for(int i=0;i<n;i++){
+        for(int j=n;j>i;--j){
+            cout<<"* ";
+       }
+        cout<<"\n";
+    }
+}
 
 int main(){
     int x;
     cin >> x;
-    square(x);
-  
+    rightTriHypRightSameNum(x);
+    rightTriHypRightDownSameNum(x);
 }
--- Patterns/patterns.cpp
+++ Patterns/patterns.cpp
@@ -18,6 +18,40 @@ void strSqr(int x){
     }
     
 }
+void rightTriHypRight(int n){
+    cout<<"we star tringle"<<"\n";
+    for (int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+}
+void rightTriHypRightNum(int n){
+    cout<<"we number tringle"<<"\n";
+    for (int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            cout<<j+1;
+        }
+        cout<<"\n";
+    }
+}
+void rightTriHypRightSameNum(int x){
+    for(int i=0;i<x;i++){
+        for (int j=0; j<i; j++){
+            cout<<i<<" ";
+        }
+        cout<<"\n";
+    }
+}
+void rightTriHypRightDownSameNum(int n){
+    for(int i=0;i<n;i++){
+        for(int j=n;j>i;--j){
+            cout<<"* ";
+       }
+        cout<<"\n";
+    }
+}
 
 
 int main(){
